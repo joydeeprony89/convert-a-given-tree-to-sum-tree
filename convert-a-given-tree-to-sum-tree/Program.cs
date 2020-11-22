21b9b6138b424108a6c2a6b91e8bb5ae77c70dd0
Jaydeep Shil <ED8A@eurofins.com>
2020-11-22 19:06:07 +0530

    Add project files.

---
+++ convert-a-given-tree-to-sum-tree/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace convert_a_given_tree_to_sum_tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = new Node(10);
+            root.left = new Node(-2);
+            root.right = new Node(6);
+            root.right.left = new Node(7);
+            root.right.right = new Node(5);
+            root.left.left = new Node(8);
+            root.left.right = new Node(-4);
+            ToSumTree(root);
+            PreOrder(root);
+        }
+
+        public class Node
+        {
+            public int val { get; set; }
+            public Node left { get; set; }
+            public Node right { get; set; }
+            public Node(int val = 0)
+            {
+                this.val = val;
+                left = right = null;
+            }
+        }
+
+        static int ToSumTree(Node root)
+        {
+            if (root == null) return 0;
+            int old_val = root.val;
+            root.val = ToSumTree(root.left) + ToSumTree(root.right);
+            return old_val + root.val;
+        }
+
+        static void PreOrder(Node root)
+        {
+            if (root == null) return;
+            Console.WriteLine(root.val);
+            PreOrder(root.left);
+            PreOrder(root.right);
+        }
+    }
+}
